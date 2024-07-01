f4ad0e21c3f6847678eea951e9b0f54e3f32f46b
KrystalYao <71368223+KrystalYao@users.noreply.github.com>
2024-07-01 18:39:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ def handle_message(event):
                             url=movie['picture'],
                             size="full",
                             aspect_mode="cover",
-                            aspect_ratio="150：100"
+                            aspect_ratio="1.15：1"
                         ),
                         body=BoxComponent(
                             layout="vertical",
