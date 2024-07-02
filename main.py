898be4a814ae94d05c9eb689d194e5a479d4c8d4
KrystalYao <71368223+KrystalYao@users.noreply.github.com>
2024-07-03 00:08:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -159,10 +159,10 @@ def handle_message(event):
             selected_region = user_state[user_id]['region']
 
             if selected_genre == "全部":
-            selected_genre = ""
+                selected_genre = ""
 
             if selected_region == "全部":
-            selected_region = ""
+                selected_region = ""
 
             # 根据用户选择的类型和地区筛选电影
             filtered_movies = movies_df[
